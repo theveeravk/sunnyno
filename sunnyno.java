a4b701645f4b0963dd5d2146e7d3f412a3c44887
theveeravk <80521071+theveeravk@users.noreply.github.com>
2021-07-20 18:06:53 +0530

    Add files via upload

---
+++ sunnyno.java
@@ -0,0 +1,23 @@
+
+public class sunnyno {
+
+	public static void main(String[] args) 
+	{
+		
+		int no = 8;
+		double sqr =Math.sqrt(no+1);
+		System.out.println(sqr);
+		if((int)(sqr)==sqr)
+		{
+			System.out.println("It is sunny no");
+			
+		}
+		else
+		{
+			System.out.println("It is not sunny no");
+		}
+		
+
+	}
+
+}
